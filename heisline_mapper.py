d465b6577ea7547fda9689db7712778790b5749d
heisenbergk <johnykypriotakis@gmail.com>
2018-02-05 08:33:50 +0200

    Bug fix

--- heisline_mapper.py
+++ heisline_mapper.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d, sproot, splrep, splev
 from mpl_toolkits.mplot3d import Axes3D
 import pyregion
-from matplotlib import axes\
+from matplotlib import axes
 from matplotlib import cm
 
 gridfolder = 'griddata/'
